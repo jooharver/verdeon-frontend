d313e6ce79eaea94e372f0a635d317c1cc352078
Eka Krisna Ferian <kresnaeka.07@gmail.com>
2025-11-13 10:13:00 +0700

    Update Global Environment : Config, globals.css, Dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**', // Izinkan semua path gambar dari Google
+      },
+    ],
+  },
 };
 
 export default nextConfig;
